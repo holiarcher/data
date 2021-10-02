9ca8bb2874c54568814f3251ebdd24a3f5832072
holiarcher <nhoj1nhoj2@gmail.com>
2021-10-02 13:21:56 +0800

    sec data

--- 资料分析.py
+++ 资料分析.py
@@ -8,3 +8,15 @@ with open('reviews.txt','r') as f:
 		if count % 1000 == 0:
 			print(len(data))
 print('there are ',len(data),'data')
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len +len(d)
+print('留言平均长度为',sum_len/len(data))
+
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new),'比数小于100')
